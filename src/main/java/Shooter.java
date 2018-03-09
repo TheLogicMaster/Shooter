d1d51051c31abe95b37cefb79a0e264f31982dfc
Justin Marentette <justinmarentette11@gmail.com>
2018-03-08 22:50:19 -0500

    Setup jmonkey sdk

--- src/main/java/Shooter.java
+++ src/main/java/Shooter.java
@@ -1,6 +1,18 @@
-public class Shooter {
+import com.jme3.app.SimpleApplication;
+
+public class Shooter extends SimpleApplication {
 
     public Shooter() {
+
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Override
+    public void simpleInitApp() {
 
     }
 }
