4fdd0c11d907f366861a53ecc288d7697a0032ee
Justin Marentette <justinmarentette11@gmail.com>
2018-03-08 23:40:19 -0500

    Render cube example code.

---
+++ src/main/java/DesktopLauncher.java
@@ -0,0 +1,7 @@
+public class DesktopLauncher {
+
+    public static void main(String[] args) {
+        Shooter shooter = new Shooter();
+        shooter.start();
+    }
+}
--- src/main/java/Shooter.java
+++ src/main/java/Shooter.java
@@ -1,4 +1,8 @@
 import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
 public class Shooter extends SimpleApplication {
 
@@ -13,6 +17,12 @@ public class Shooter extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);
     }
 }
